4eb96f5caaf1f570ff641aa9e7bf47b26ea74e5d
HIMESH <himeshrathee@gmail.com>
2026-08-28 01:36:01 +0530

    Time: 8 ms (36.28%), Space: 102 MB (85.41%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        
+        int r= grid.size();
+        int c= grid[0].size();
+        
+       int peri = 0 ;  
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]==1){
+                   if(i== 0 || grid[i-1][j] != 1){
+                       peri+= 1; 
+                   }
+                    if(j==0 || grid[i][j-1] ==0){
+                        peri+= 1 ;
+                    }
+                    if(i == r-1 || grid[i+1][j] == 0){
+                        peri+= 1; 
+                    }
+                    if(j== c-1 || grid[i][j+1] == 0 ){
+                        peri+=1; 
+                    }
+                }
+                
+            }
+           
+        }
+       return peri ; 
+
+    }
+};
